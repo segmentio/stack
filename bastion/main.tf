484fbf0aa0984b4ea889512d71beb355b76386e9
Vincent Caggiari <vincent@edencia.com>
2016-12-12 00:11:23 +0100

    Revert "updated bastion to use xenial AMI"
    This reverts commit 4749726249a06cf3a6863cf187d0866483148f73.

--- bastion/main.tf
+++ bastion/main.tf
@@ -53,7 +53,7 @@ variable "environment" {
 module "ami" {
   source        = "github.com/terraform-community-modules/tf_aws_ubuntu_ami/ebs"
   region        = "${var.region}"
-  distribution  = "xenial"
+  distribution  = "trusty"
   instance_type = "${var.instance_type}"
 }
 
