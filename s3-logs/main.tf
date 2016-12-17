a7fd045f27816d3de2e87740743524b3145de90d
Vincent Caggiari <vincent@edencia.com>
2016-12-17 22:47:06 +0100

    added logs expiration

--- s3-logs/main.tf
+++ s3-logs/main.tf
@@ -7,6 +7,14 @@ variable "environment" {
 variable "account_id" {
 }
 
+variable "logs_expiration_enabled" {
+  default = false
+}
+
+variable "logs_expiration_days" {
+  default = 30
+}
+
 data "template_file" "policy" {
   template = "${file("${path.module}/policy.json")}"
 
@@ -18,6 +26,16 @@ data "template_file" "policy" {
 
 resource "aws_s3_bucket" "logs" {
   bucket = "${var.name}-${var.environment}-logs"
+
+  lifecycle_rule {
+    id = "logs-expiration"
+    prefix = ""
+    enabled = "${var.logs_expiration_enabled}"
+
+    expiration {
+      days = "${var.logs_expiration_days}"
+    }
+  }
 
   tags {
     Name        = "${var.name}-${var.environment}-logs"
