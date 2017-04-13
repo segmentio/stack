a72256e99a4cad2eda1905c7c0d1d4c2db63362c
Nathaniel Schweinberg <natanielks@gmail.com>
2017-04-13 14:40:07 -0700

    output ecs task definition revision from task module

--- task/main.tf
+++ task/main.tf
@@ -113,3 +113,8 @@ output "name" {
 output "arn" {
   value = "${aws_ecs_task_definition.main.arn}"
 }
+
+// The revision number of the task definition
+output "revision" {
+  value = "${aws_ecs_task_definition.main.revision}"
+}
