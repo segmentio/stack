1ec066bcc619f49cd8df447913cc4095d0c10134
Nathaniel Schweinberg <natanielks@gmail.com>
2017-04-21 15:09:52 -0700

    enable web-service module to track latest ecs task revision

--- web-service/main.tf
+++ web-service/main.tf
@@ -143,6 +143,9 @@ resource "aws_ecs_service" "main" {
   deployment_minimum_healthy_percent = "${var.deployment_minimum_healthy_percent}"
   deployment_maximum_percent         = "${var.deployment_maximum_percent}"
 
+  # Track the latest ACTIVE revision
+  task_definition = "${module.task.name}:${max("${module.task.revision}", "${data.aws_ecs_task_definition.task.revision}")}"
+
   load_balancer {
     elb_name       = "${module.elb.id}"
     container_name = "${module.task.name}"
@@ -152,6 +155,10 @@ resource "aws_ecs_service" "main" {
   lifecycle {
     create_before_destroy = true
   }
+}
+
+data "aws_ecs_task_definition" "task" {
+  task_definition = "${module.task.name}"
 }
 
 module "task" {
