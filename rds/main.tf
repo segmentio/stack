6c1639cbaf858d4b59d31a208dc5c74738762d44
Dominic Barnes <dominic@dbarnes.info>
2017-05-16 16:55:15 -0700

    feat(rds): update addr output to use engine as scheme, add url output that includes db suffix

--- rds/main.tf
+++ rds/main.tf
@@ -163,5 +163,9 @@ resource "aws_db_instance" "main" {
 }
 
 output "addr" {
-  value = "postgres://${aws_db_instance.main.username}:${aws_db_instance.main.password}@${aws_db_instance.main.endpoint}"
+  value = "${aws_db_instance.main.engine}://${aws_db_instance.main.username}:${aws_db_instance.main.password}@${aws_db_instance.main.endpoint}"
+}
+
+output "url" {
+  value = "${aws_db_instance.main.engine}://${aws_db_instance.main.username}:${aws_db_instance.main.password}@${aws_db_instance.main.endpoint}/${aws_db_instance.main.database}"
 }
