b449944e02618829570c9c84f273c1e026c529e4
cardboardboxb <paulzhao99@gmail.com>
2017-12-01 11:53:18 -0800

    remove extra vars as a result of importing from service(elb), reorder vars to be more sensible

--- service-tg/main.tf
+++ service-tg/main.tf
@@ -17,22 +17,26 @@
  * Required Variables.
  */
 
-variable "environment" {
-  description = "Environment tag, e.g prod"
+variable "name" {
+   description = "The service name, if empty the service name is defaulted to the image name"
+   default     = ""
+}
+
+variable "cluster" {
+  description = "The cluster name or ARN"
+}
+
+variable "iam_role" {
+  description = "IAM Role ARN to use"
+}
+
+variable "container_port" {
+  description = "The container port"
+  default     = 3000
 }
 
 variable "image" {
   description = "The docker image name, e.g nginx"
-}
-
-variable "name" {
-  description = "The service name, if empty the service name is defaulted to the image name"
-  default     = ""
-}
-
-variable "protocol" {
-  description = "Protocol to use, HTTP or TCP"
-  default = "HTTP"
 }
 
 variable "version" {
@@ -40,42 +44,60 @@ variable "version" {
   default     = "latest"
 }
 
-variable "subnet_ids" {
-  description = "Comma separated list of subnet IDs that will be passed to the tg-listner module"
+variable "protocol" {
+  description = "Protocol to use, HTTP or TCP"
+  default = "HTTP"
 }
 
-variable "security_groups" {
-  description = "Comma separated list of security group IDs that will be passed to the tg-listner module"
+variable "vpc_id" {
+  description = "The id of the VPC."
 }
 
 variable "port" {
   description = "The container host port"
 }
 
-variable "cluster" {
-  description = "The cluster name or ARN"
+variable "priority" {
+  description = "The priority for the rule. A listener can't have multiple rules with the same priority"
 }
 
-variable "log_bucket" {
-  description = "The S3 bucket ID to use for the tg-listner"
+variable "condition_values" {
+  description = "Listner rule condition. This should include the service name and the environment, there must be no trailing period (.); omit the root domain. For example: community.dev"
+  type = "string"
 }
 
-variable "iam_role" {
-  description = "IAM Role ARN to use"
+variable "listener_arn_80" {
+  description = "arn of an aws_alb_listener"
+  type = "string"
+}
+
+variable "listener_arn_443" {
+  description = "arn of an aws_alb_listener"
+  type = "string"
+}
+
+variable "tags" {
+  description = "tags for the distribution"
+  type        = "map"
+  description = "tags, just tags"
 }
 
 /**
  * Options.
  */
-
-variable "healthcheck" {
-  description = "Path to a healthcheck endpoint"
-  default     = "/"
+variable "desired_count" {
+ description = "The desired count"
+ default     = 2
 }
 
-variable "container_port" {
-  description = "The container port"
-  default     = 3000
+variable "deployment_minimum_healthy_percent" {
+ description = "lower limit (% of desired_count) of # of running tasks during a deployment"
+ default     = 100
+}
+
+variable "deployment_maximum_percent" {
+ description = "upper limit (% of desired_count) of # of running tasks during a deployment"
+ default     = 200
 }
 
 variable "command" {
@@ -88,11 +110,6 @@ variable "env_vars" {
   default     = "[]"
 }
 
-variable "desired_count" {
-  description = "The desired count"
-  default     = 2
-}
-
 variable "memory" {
   description = "The number of MiB of memory to reserve for the container"
   default     = 512
@@ -103,87 +120,54 @@ variable "cpu" {
   default     = 512
 }
 
-variable "deployment_minimum_healthy_percent" {
-  description = "lower limit (% of desired_count) of # of running tasks during a deployment"
-  default     = 100
-}
-
-variable "deployment_maximum_percent" {
-  description = "upper limit (% of desired_count) of # of running tasks during a deployment"
-  default     = 200
-}
-
-variable "vpc_id" {
-  description = "The id of the VPC."
-}
-
 variable "health_check_protocol" {
   description = "The protocol that the load balancer uses when performing health checks on the targets. Accepts HTTP, HTTPS "
   type = "string"
   default = "HTTP"
 }
+
 variable "health_check_path" {
   description = "The ping path destination where Elastic Load Balancing sends health check requests"
   type = "string"
-  default = "/api/Heartbeats"
+  default = "/"
 }
+
 variable "health_check_port" {
   description = "The port to use to connect with the target. Valid values are either ports 1-65536, or traffic-port"
   type = "string"
   default = "traffic-port"
 }
+
 variable "health_check_healthy_threshold" {
   description = "The number of consecutive successful health checks that are required before an unhealthy target is considered healthy"
   default = 2
 }
+
 variable "health_check_unhealthy_threshold" {
   description = " The number of consecutive failed health checks that are required before a target is considered unhealthy"
   default = 2
 }
+
 variable "health_check_timeout" {
   description = "The number of seconds to wait for a response before considering that a health check has failed"
   default = 5
 }
+
 variable "health_check_interval" {
   description = "The approximate number of seconds between health checks for an individual target"
   default = 30
 }
+
 variable "health_check_matcher" {
   description = "The HTTP codes that a healthy target uses when responding to a health check. HTTP codes must be in Matcher data type"
   type = "string"
   default = "200-399"
 }
 
-variable "tags" {
-  description = "tags for the distribution"
-  type        = "map"
-  description = "tags, just tags"
-}
-
-//additional listener specific vars
-variable "priority" {
-  description = "The priority for the rule. A listener can't have multiple rules with the same priority"
-}
-
 variable "condition_field" {
   description = "The name of the field. Must be one of path-pattern for path based routing or host-header for host based routing. Accepts path-pattern, host-header"
   type = "string"
   default = "host-header"
-}
-
-variable "condition_values" {
-  description = "This should include the service name and the environment, there must be no trailing period (.); omit the root domain. For example: community.dev"
-  type = "string"
-}
-
-variable "listener_arn_80" {
-  description = "arn of an aws_alb_listener"
-  type = "string"
-}
-
-variable "listener_arn_443" {
-  description = "arn of an aws_alb_listener"
-  type = "string"
 }
 
 /**
@@ -236,7 +220,6 @@ module "tg-listner" {
   source = "../tg-listner"
 
   name                             = "${module.task.name}"
-  environment                      = "${var.environment}"
   port                             = "${var.port}"
   protocol                         = "${var.protocol}"
   vpc_id                           = "${var.vpc_id}"
@@ -251,9 +234,9 @@ module "tg-listner" {
   priority                         = "${var.priority}"
   condition_field                  = "${var.condition_field}"
   condition_values                 = "${var.condition_values}"
-  tags                             = "${var.tags}"
   listener_arn_80                  = "${var.listener_arn_80}"
   listener_arn_443                 = "${var.listener_arn_443}"
+  tags                             = "${var.tags}"
 }
 
 /**
--- tg-listner/main.tf
+++ tg-listner/main.tf
@@ -3,12 +3,11 @@
  * It is used by the service-tg module.
  */
 
+/**
+* Required Variables.
+*/
 variable "name" {
   description = "target group name, e.g cdn"
-}
-
-variable "environment" {
-  description = "Environment tag, e.g prod"
 }
 
 variable "port" {
@@ -24,63 +23,10 @@ variable "vpc_id" {
   description = "The ID of the VPC in which your targets are located"
 }
 
-variable "health_check_protocol" {
-  description = "The protocol that the load balancer uses when performing health checks on the targets. Accepts HTTP, HTTPS "
-  type = "string"
-  default = "HTTP"
-}
-variable "health_check_path" {
-  description = "The ping path destination where Elastic Load Balancing sends health check requests"
-  type = "string"
-  default = "/api/Heartbeats"
-}
-variable "health_check_port" {
-  description = "The port to use to connect with the target. Valid values are either ports 1-65536, or traffic-port"
-  type = "string"
-  default = "traffic-port"
-}
-variable "health_check_healthy_threshold" {
-  description = "The number of consecutive successful health checks that are required before an unhealthy target is considered healthy"
-  default = 2
-}
-variable "health_check_unhealthy_threshold" {
-  description = " The number of consecutive failed health checks that are required before a target is considered unhealthy"
-  default = 2
-}
-variable "health_check_timeout" {
-  description = "The number of seconds to wait for a response before considering that a health check has failed"
-  default = 5
-}
-variable "health_check_interval" {
-  description = "The approximate number of seconds between health checks for an individual target"
-  default = 30
-}
-variable "health_check_matcher" {
-  description = "The HTTP codes that a healthy target uses when responding to a health check. HTTP codes must be in Matcher data type"
-  type = "string"
-  default = "200-399"
-}
-
 variable "tags" {
   description = "tags for the distribution"
   type        = "map"
   description = "tags, just tags"
-}
-
-//additional listener specific vars
-variable "priority" {
-  description = "The priority for the rule. A listener can't have multiple rules with the same priority"
-}
-
-variable "condition_field" {
-  description = "The name of the field. Must be one of path-pattern for path based routing or host-header for host based routing. Accepts path-pattern, host-header"
-  type = "string"
-  default = "host-header"
-}
-
-variable "condition_values" {
-  description = "This should include the service name and the environment, there must be no trailing period (.); omit the root domain. For example: community.dev"
-  type = "string"
 }
 
 variable "listener_arn_80" {
@@ -88,9 +34,71 @@ variable "listener_arn_80" {
   type = "string"
 }
 
+variable "priority" {
+  description = "The priority for the rule. A listener can't have multiple rules with the same priority"
+}
+
+variable "condition_values" {
+  description = "This should include the service name and the environment, there must be no trailing period (.); omit the root domain. For example: community.dev"
+  type = "string"
+}
+
 variable "listener_arn_443" {
   description = "arn of an aws_alb_listener"
   type = "string"
+}
+
+/**
+ * Options.
+ */
+variable "health_check_protocol" {
+  description = "The protocol that the load balancer uses when performing health checks on the targets. Accepts HTTP, HTTPS "
+  type = "string"
+  default = "HTTP"
+}
+
+variable "health_check_path" {
+  description = "The ping path destination where Elastic Load Balancing sends health check requests"
+  type = "string"
+  default = "/"
+}
+
+variable "health_check_port" {
+  description = "The port to use to connect with the target. Valid values are either ports 1-65536, or traffic-port"
+  type = "string"
+  default = "traffic-port"
+}
+
+variable "health_check_healthy_threshold" {
+  description = "The number of consecutive successful health checks that are required before an unhealthy target is considered healthy"
+  default = 2
+}
+
+variable "health_check_unhealthy_threshold" {
+  description = " The number of consecutive failed health checks that are required before a target is considered unhealthy"
+  default = 2
+}
+
+variable "health_check_timeout" {
+  description = "The number of seconds to wait for a response before considering that a health check has failed"
+  default = 5
+}
+
+variable "health_check_interval" {
+  description = "The approximate number of seconds between health checks for an individual target"
+  default = 30
+}
+
+variable "health_check_matcher" {
+  description = "The HTTP codes that a healthy target uses when responding to a health check. HTTP codes must be in Matcher data type"
+  type = "string"
+  default = "200-399"
+}
+
+variable "condition_field" {
+  description = "The name of the field. Must be one of path-pattern for path based routing or host-header for host based routing. Accepts path-pattern, host-header"
+  type = "string"
+  default = "host-header"
 }
 
 /**
